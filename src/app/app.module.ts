5cd5a30920e304d314f2eb5fc71c98a7dec8009f
Süleyman Aydın <suleyman.aydin@matriksdata.com>
2016-12-21 17:27:58 +0300

    material add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,16 @@ import { AppComponent } from './app.component';
 import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import { TasksModule } from '../tasks/index';
 import { RouterModule } from '@angular/router';
-
+import { MaterialModule } from '@angular/material';
 
 
 
 const fbConfig = {
- 
+  apiKey: "AIzaSyB5ybQVnAyiUgw84t8PBBwRYPPT44wsXaw",
+  authDomain: "ngfb-44987.firebaseapp.com",
+  databaseURL: "https://ngfb-44987.firebaseio.com",
+  storageBucket: "ngfb-44987.appspot.com",
+  messagingSenderId: "498586561793"
 };
 
 const fbAuthConfig  = {
@@ -30,7 +34,8 @@ const fbAuthConfig  = {
     HttpModule,
     RouterModule.forRoot([], {useHash: false}),
     AngularFireModule.initializeApp(fbConfig, fbAuthConfig ),
-    TasksModule
+    TasksModule,
+    MaterialModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
